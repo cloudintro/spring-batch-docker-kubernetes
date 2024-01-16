56b3f8d0ce0bec202fcf4ce49602498e799ee704
Sushil Prasad <cse.sushil@gmail.com>
2024-01-17 00:04:19 +0530

    addred RSocket message server and client

--- task-processor/src/main/java/com/cloudcode/taskprocessor/controller/TaskController.java
+++ task-processor/src/main/java/com/cloudcode/taskprocessor/controller/TaskController.java
@@ -12,18 +12,20 @@ import reactor.core.publisher.Mono;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Log4j2
 @RestController
 public class TaskController {
 
+    @Autowired
     private TaskService taskService;
 
-    private TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
-
+    // HTTP Rest endpoints
     @PostMapping("/task")
     public Mono<TaskInfo> saveTask(@RequestBody TaskInfo taskInfo) {
         log.info("save task {}", taskInfo);
@@ -31,9 +33,28 @@ public class TaskController {
     }
 
     @GetMapping("/task")
-    public Flux<TaskInfo> getTaskStatus(
+    public Flux<TaskInfo> getTask(
             @RequestParam(required = false, name = "task-id") Long taskId,
             @RequestParam(required = false, name = "task-name") String taskName) {
         return taskService.getTaskStatus(taskId, taskName);
     }
+
+    /*
+     * For demo this rest endpoint is creted in same service controller
+     * In real project this will be there in different client service
+     * move to task manager
+     */
+    private RSocketRequester rSocketRequester;
+
+    public TaskController(RSocketRequester.Builder builder,
+            @Value("${spring.rsocket.server.port:7000}") Integer rSocketPort) {
+        rSocketRequester = builder.tcp("localhost", rSocketPort);
+    }
+
+    @GetMapping("/stream")
+    public ResponseEntity<Flux<TaskInfo>> streamTask() {
+        Flux<TaskInfo> tasks = rSocketRequester.route("task-stream").retrieveFlux(TaskInfo.class);
+        return ResponseEntity.ok().body(tasks);
+    }
+
 }
